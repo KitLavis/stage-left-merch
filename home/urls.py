e0ee78ad3adc580f4eb635d77737b6d96020fe5e
Kit Lavis <kcjlavis@gmail.com>
2024-02-14 13:45:34 +0000

    Add hero text to index.html

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='home')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += staticfiles_urlpatterns()
