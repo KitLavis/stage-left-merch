8d9dc37df84094209e279297dbcf1557dbf590e8
Kit Lavis <kcjlavis@gmail.com>
2024-02-15 10:38:44 +0000

    Product model and admin updated with slug

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('sku', 'name',)}
+
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'featured_image',
+        'created_on',
+    )
+
+    ordering = ('sku',)
+
+admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from cloudinary.models import CloudinaryField
 
@@ -19,9 +20,12 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=250, null=True, blank=True)
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
