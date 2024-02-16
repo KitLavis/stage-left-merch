d5ea7a7c7453ba91476c92dbe11f18df24dbad4f
Kit Lavis <kcjlavis@gmail.com>
2024-02-16 09:14:43 +0000

    Product detail view, url and template added

--- products/views.py
+++ products/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def all_products(request):
 
-    products = Product.objects.all()
+    queryset = Product.objects.filter(status=1).all()
 
     context = {
-        'products': products,
+        'products': queryset,
     }
 
     return render(request, 'products/all_products.html', context)
+
+
+def product_detail(request, slug):
+
+    queryset = Product.objects.filter(status=1)
+    product = get_object_or_404(queryset, slug=slug)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
