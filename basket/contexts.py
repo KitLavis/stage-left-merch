c627a7963f0e7dd3115783ca00aae9ec51d5468f
Kit Lavis <kcjlavis@gmail.com>
2024-02-19 12:14:45 +0000

    Session data deleted must fix

--- basket/contexts.py
+++ basket/contexts.py
@@ -10,15 +10,27 @@ def basket_contents(request):
     product_count = 0
     basket = request.session.get('basket', {})
 
-    for product_id, quantity in basket.items():
-        product = get_object_or_404(Product, pk=product_id)
-        total += quantity * product.price
-        product_count += quantity
-        basket_items.append({
-            'product_id': product_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for product_id, product_data in basket.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=product_id)
+            total += product_data * product.price
+            product_count += product_data
+            basket_items.append({
+                'product_id': product_id,
+                'quantity': product_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=product_id)
+            for size, quantity in product_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                basket_items.append({
+                    'product_id': product_id,
+                    'quantity': product_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_POSTAGE_THRESHOLD:
         postage = total * Decimal(settings.STANDARD_POSTAGE_PERCENTAGE / 100)
