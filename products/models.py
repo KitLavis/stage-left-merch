b7d291a3a226f8abe4e75d02f4b3ac8a3e13732c
Kit Lavis <kcjlavis@gmail.com>
2024-02-19 11:57:30 +0000

    Sizes added to model and product detail template

--- products/models.py
+++ products/models.py
@@ -37,6 +37,7 @@ class Product(models.Model):
     name = models.CharField(max_length=250)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
