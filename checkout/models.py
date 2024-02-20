955fa42116b79a1eb8536f050c24eeabfcc9ac53
Kit Lavis <kcjlavis@gmail.com>
2024-02-20 09:29:31 +0000

    Create order reference generator

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from basket.models import Basket
+from .utils import unique_order_ref_generator
 
-# Create your models here.
+
+class Order(models.Model):
+    order_ref = models.CharField()
+
+    def __str__(self):
+        return self.order_ref
+
+
+def pre_save_create_order_ref(sender, instance, *args, **kwargs):
+    if not instance.order_ref:
+        instance.order_ref= unique_order_ref_generator(instance)
+
+
+pre_save.connect(pre_save_create_order_ref, sender=Order)
