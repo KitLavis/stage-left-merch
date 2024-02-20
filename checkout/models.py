3265c68b537ad18fa85c8454b511554cafef21aa
Kit Lavis <kcjlavis@gmail.com>
2024-02-20 11:15:22 +0000

    Checkout models complete and migrated

--- checkout/models.py
+++ checkout/models.py
@@ -2,14 +2,13 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-
 from products.models import Product
 
 
 class Order(models.Model):
     order_ref = models.CharField(max_length=120, blank=False, unique=True, editable=False)
-    first_name = models.CharField(max_legnth=25, null=False, blank=False)
-    last_name = models.CharField(max_legnth=25, null=False, blank=False)
+    first_name = models.CharField(max_length=25, null=False, blank=False)
+    last_name = models.CharField(max_length=25, null=False, blank=False)
     email = models.EmailField(max_length=250, null=False, blank=False)
     mobile_number = models.CharField(max_length=25, blank=True, null=True)
     house_name = models.CharField(max_length=25, blank=True, null=True)
@@ -35,6 +34,16 @@ class Order(models.Model):
     def __str__(self):
         return self.order_ref
 
+    def update_total(self):
+
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_POSTAGE_THRESHOLD:
+            self.postage_packaging = self.order_total * settings.STANDARD_POSTAGE_PERCENTAGE / 100
+        else:
+            self.postage_packaging = 0
+        self.grand_total = self.order_total + self.postage_packaging
+        self.save()
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
@@ -44,10 +53,7 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
-        """
-        Override the original save method to set the lineitem total
-        and update the order total.
-        """
+
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
