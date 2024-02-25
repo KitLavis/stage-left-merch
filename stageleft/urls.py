bdb984d81b1069fe943c52334c56ecbab6b63155
Kit Lavis <kcjlavis@gmail.com>
2024-02-25 12:33:40 +0000

    User view, url and template setup

--- stageleft/urls.py
+++ stageleft/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('products/', include('products.urls')),
     path('checkout/', include('checkout.urls')),
+    path('user/', include('user.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
