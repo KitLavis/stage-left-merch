ea416e7e45741c4a7bdf1bea8f86564fbf246fa3
Kit Lavis <kcjlavis@gmail.com>
2024-02-26 12:56:06 +0000

    ProfileForm now saves details

--- user/views.py
+++ user/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
 from .forms import ProfileForm
+
 
 
 def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.add_message(
+            request,
+            messages.SUCCESS,
+            'Your details were updated successfully!'
+            )
 
     form = ProfileForm(instance=profile)
     orders = profile.orders.all()
