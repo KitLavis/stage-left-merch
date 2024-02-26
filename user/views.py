e56f7f0e0224aa31319e3428c0644111688381e6
Kit Lavis <kcjlavis@gmail.com>
2024-02-26 10:04:11 +0000

    Profile form written and rendered on profile page

--- user/views.py
+++ user/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import ProfileForm
 
 
 def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = ProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'user/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
