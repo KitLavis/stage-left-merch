f88efb49a448fe9f923b90c6ec686159435839fb
Kit Lavis <kcjlavis@gmail.com>
2024-03-07 14:47:18 +0000

    Adjustments for deployment

--- stageleft/settings.py
+++ stageleft/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-kitlavis-stageleftmerch-dfadd5f4vsn.ws-eu108.gitpod.io',
