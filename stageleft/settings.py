d9d7d6b9e20571d55d78761a6b544b9709dabea4
Kit Lavis <kcjlavis@gmail.com>
2024-03-07 08:36:23 +0000

    Basket styling updated

--- stageleft/settings.py
+++ stageleft/settings.py
@@ -24,12 +24,11 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-kitlavis-stageleftmerch-dfadd5f4vsn.ws-eu108.gitpod.io',
