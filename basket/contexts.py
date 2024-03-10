5503a8ea31c5906450587ef57c1698fb31ab55bd
Kit Lavis <kcjlavis@gmail.com>
2024-03-10 15:21:57 +0000

    Basket docstrings and second lint

--- basket/contexts.py
+++ basket/contexts.py
@@ -3,34 +3,42 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-def basket_contents(request):
 
+def basket_contents(request):
+    """
+    Allows the contents of the session's basket
+    to be accessed from everywhere
+    """
     basket_items = []
     total = 0
     product_count = 0
-    basket = request.session.get('basket', {})
+    basket = request.session.get("basket", {})
 
     for product_id, product_data in basket.items():
         if isinstance(product_data, int):
             product = get_object_or_404(Product, pk=product_id)
             total += product_data * product.price
             product_count += product_data
-            basket_items.append({
-                'product_id': product_id,
-                'quantity': product_data,
-                'product': product,
-            })
+            basket_items.append(
+                {
+                    "product_id": product_id,
+                    "quantity": product_data,
+                    "product": product,
+                }
+            )
         else:
             product = get_object_or_404(Product, pk=product_id)
-            for size, quantity in product_data['items_by_size'].items():
+            for size, quantity in product_data["items_by_size"].items():
                 total += quantity * product.price
                 product_count += quantity
-                basket_items.append({
-                    'product_id': product_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'size': size,
-                })
+                basket_items.append(
+                    {
+                        "product_id": product_id,
+                        "quantity": quantity,
+                        "product": product,
+                        "size": size,
+                    }
+                )
 
     if total < settings.FREE_POSTAGE_THRESHOLD:
         postage = total * Decimal(settings.STANDARD_POSTAGE_PERCENTAGE / 100)
@@ -38,17 +46,17 @@ def basket_contents(request):
     else:
         postage = 0
         free_postage_delta = 0
-    
+
     grand_total = postage + total
-    
+
     context = {
-        'basket_items': basket_items,
-        'total': total,
-        'product_count': product_count,
-        'postage': postage,
-        'free_postage_delta': free_postage_delta,
-        'free_postage_threshold': settings.FREE_POSTAGE_THRESHOLD,
-        'grand_total': grand_total,
+        "basket_items": basket_items,
+        "total": total,
+        "product_count": product_count,
+        "postage": postage,
+        "free_postage_delta": free_postage_delta,
+        "free_postage_threshold": settings.FREE_POSTAGE_THRESHOLD,
+        "grand_total": grand_total,
     }
 
     return context
--- basket/views.py
+++ basket/views.py
@@ -3,57 +3,59 @@ from django.contrib import messages
 
 
 def shopping_basket(request):
-
-    return render(request, 'basket/basket.html')
+    """
+    Returns the session's basket
+    """
+    return render(request, "basket/basket.html")
 
 
 def add_to_basket(request, product_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """Add a quantity of the specified product to the basket"""
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
     size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    basket = request.session.get('basket', {})
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+    basket = request.session.get("basket", {})
 
     if size:
         if product_id in list(basket.keys()):
-            if size in basket[product_id]['items_by_size'].keys():
-                basket[product_id]['items_by_size'][size] += quantity
+            if size in basket[product_id]["items_by_size"].keys():
+                basket[product_id]["items_by_size"][size] += quantity
             else:
-                basket[product_id]['items_by_size'][size] = quantity
+                basket[product_id]["items_by_size"][size] = quantity
         else:
-            basket[product_id] = {'items_by_size': {size: quantity}}
+            basket[product_id] = {"items_by_size": {size: quantity}}
     else:
         if product_id in list(basket.keys()):
             basket[product_id] += quantity
         else:
             basket[product_id] = quantity
 
-    request.session['basket'] = basket
-    messages.add_message(
-            request,
-            messages.SUCCESS,
-            'The item was added to your basket'
-            )
+    request.session["basket"] = basket
+    messages.add_message(request, messages.SUCCESS,
+                         "The item was added to your basket")
     return redirect(redirect_url)
 
 
 def modify_basket(request, product_id):
-
-    quantity = int(request.POST.get('quantity'))
+    """
+    Allows the user to modify that quantity of
+    a product in the basket
+    """
+    quantity = int(request.POST.get("quantity"))
     size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    basket = request.session.get('basket', {})
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+    basket = request.session.get("basket", {})
 
     if size:
         if quantity > 0:
-            basket[product_id]['items_by_size'][size] = quantity
+            basket[product_id]["items_by_size"][size] = quantity
         else:
-            del basket[product_id]['items_by_size'][size]
-            if not basket[product_id]['items_by_size']:
+            del basket[product_id]["items_by_size"][size]
+            if not basket[product_id]["items_by_size"]:
                 basket.pop(product_id)
     else:
         if quantity > 0:
@@ -61,43 +63,35 @@ def modify_basket(request, product_id):
         else:
             basket.pop(product_id)
 
-    request.session['basket'] = basket
-    messages.add_message(
-            request,
-            messages.SUCCESS,
-            'Basket updated succesfully'
-            )
-    return redirect(reverse('shopping_basket'))
+    request.session["basket"] = basket
+    messages.add_message(request, messages.SUCCESS,
+                         "Basket updated succesfully")
+    return redirect(reverse("shopping_basket"))
 
 
 def remove_from_basket(request, product_id):
-    """Remove the item from the basket """
+    """Remove the item from the basket"""
 
     try:
         size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
-        basket = request.session.get('basket', {})
+        if "product_size" in request.POST:
+            size = request.POST["product_size"]
+        basket = request.session.get("basket", {})
 
         if size:
-            del basket[product_id]['items_by_size'][size]
-            if not basket[product_id]['items_by_size']:
+            del basket[product_id]["items_by_size"][size]
+            if not basket[product_id]["items_by_size"]:
                 basket.pop(product_id)
         else:
             basket.pop(product_id)
 
-        request.session['basket'] = basket
-        messages.add_message(
-            request,
-            messages.SUCCESS,
-            'The item has been removed from your basket'
-            )
+        request.session["basket"] = basket
+        messages.add_message(request, messages.SUCCESS,
+                             "The item has been removed from your basket")
         return HttpResponse(status=200)
 
     except Exception as e:
         messages.add_message(
-            request,
-            messages.ERROR,
-            'Something went wrong. Please try again'
-            )
+            request, messages.ERROR, "Something went wrong. Please try again"
+        )
         return HttpResponse(status=500)
