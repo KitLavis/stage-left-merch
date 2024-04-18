c204fbf68ce6c9e367f7027cf6202383e085dc73
Kit Lavis <kcjlavis@gmail.com>
2024-04-18 08:58:21 +0000

    Real emails/smtp added to settings

--- stageleft/settings.py
+++ stageleft/settings.py
@@ -113,8 +113,6 @@ WSGI_APPLICATION = 'stageleft.wsgi.application'
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -193,4 +191,15 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
-OUR_EMAIL_ADDRESS = 'stageleft@readonly.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    OUR_EMAIL_ADDRESS = 'stageleft@readonly.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    OUR_EMAIL_ADDRESS = os.environ.get('EMAIL_HOST_USER')
